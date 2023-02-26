08e5dfba9b3c930fc3270385cd943317a8f9f5e5
Mingyu Liu <mingyuliu88@gmail.com>
2023-02-26 13:46:31 -0800

    Day 7: [crud-movie-api] REST api, postman

--- crud-movie-api/main.go
+++ crud-movie-api/main.go
@@ -41,6 +41,36 @@ func getMovie(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func createMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+
+	var movie Movie
+	_ = json.NewDecoder(req.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+
+	movies = append(movies, movie)
+	json.NewEncoder(res).Encode(movie)
+}
+
+func updateMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(req)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			
+			var movie Movie
+			_ = json.NewDecoder(req.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+
+			json.NewEncoder(res).Encode(movie)
+			return
+		}
+	}
+}
+
 func deleteMovie(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(req)
